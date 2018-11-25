707efd07a3f1529e6b71f9b6ca00b26431aa336a
xiongqi <xiongqi@dianwoda.com>
2018-11-25 15:18:28 +0800

    desc

--- library.js
+++ library.js
@@ -1,10 +1,9 @@
 const Plugin = {}
 
-Plugin.log = function(data) {
-  const {
-    uid,
-    req
-  } = data
+// 当 {"hook": "action:user.loggedIn"} 触发时调用的方法
+Plugin.log = function (data) {
+  // 使用 hook 传入的数据中的 uid
+  const { uid } = data
   console.log('------------------- 欢迎登录,用户号:', uid)
 }
 
